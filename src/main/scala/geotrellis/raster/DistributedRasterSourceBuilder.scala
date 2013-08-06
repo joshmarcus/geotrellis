28287602ef2648116d7d2971e675e4d7e6db1f64
Josh Marcus <jmarcus@azavea.com>
2013-08-06 17:36:14 -0400

    Integrate RasterDefinitions into builders.

--- src/main/scala/geotrellis/raster/DistributedRasterSourceBuilder.scala
+++ src/main/scala/geotrellis/raster/DistributedRasterSourceBuilder.scala
@@ -3,12 +3,25 @@ package geotrellis.raster
 import geotrellis._
 
 class DistributedRasterSourceBuilder extends RasterSourceBuilder[DistributedRasterSource] {
-  def result = new DistributedRasterSource(null)
   var _dataDefinition:Op[RasterDefinition] = null
 
-/** As seen from class DistributedRasterSourceBuilder, the missing signatures are as follows.  
- *  *  For convenience, these are usable as stub implementations.  */  
-  def setDataDefinition(dfn: geotrellis.Operation[geotrellis.DataDefinition]): this.type = { this }  
-  def setOp(op: Seq[geotrellis.Operation[geotrellis.Raster]]): this.type = { this } 	
-  def setRasterDefinition(dfn: geotrellis.Operation[geotrellis.RasterDefinition]): this.type = this 
+  def setOp(op: Op[Seq[Op[Raster]]]): this.type = {
+    this.op = op
+    this 
+  }
+  
+  def setRasterDefinition(dfn: geotrellis.Operation[geotrellis.RasterDefinition]): this.type = {
+    this._dataDefinition = dfn
+    this
+  }
+
+  def result = new DistributedRasterSource(_dataDefinition)
+
+}
+
+object DistributedRasterSourceBuilder {
+  def apply(rasterSource:RasterSource) = {
+    val builder = new DistributedRasterSourceBuilder()
+    builder.setRasterDefinition(rasterSource.rasterDefinition)
+  }
 }
